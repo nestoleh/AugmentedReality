4ec12878809e8e960055b21efc1d5f0adeb08740
nestoleh <nestoleh@gmail.com>
2015-11-24 20:19:50 +0200

    Добавлена реализация отслеживания камеры и реакция на маркер

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
-  
-});
+if (navigator.webkitGetUserMedia) {  
+  navigator.webkitGetUserMedia(
+    // Первый параметр - так называемые constraints, ограничения;
+    // в данном случае - предпочтительные параметры запрашиваемого потока:
+    {
+      audio: true,  // запрашиваем и аудио,...
+      video: true   // ...и видео
+    },
+    // Второй параметр - функция, вызываемая в случае успеха
+    function(stream) { // в качестве параметра передается объект LocalMediaStream
+      console.log('Stream:', stream);
+    },
+    // Третий параметр - функция, вызываемая в случае ошибки
+    function(error) { // в качестве параметра передается объект с ошибкой
+      console.log('Error:', error);
+    }
+  );
+} else {
+  console.log('navigator.webkitGetUserMedia not supported. Are you using latest Chrome/Chromium?');
+}
